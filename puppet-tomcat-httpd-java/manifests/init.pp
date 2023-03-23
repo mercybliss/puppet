66a601cdb54d32cb4d7fdcdbc8faaa03833f6fef
mercybliss <47721889+mercybliss@users.noreply.github.com>
2023-03-23 13:19:12 -0500

    Delete puppet-tomcat-httpd-java/manifests directory

--- puppet-tomcat-httpd-java/manifests/init.pp
+++
@@ -1,5 +0,0 @@
-class puppet-tomcat-httpd-java {
-  include puppet-tomcat-httpd-java::java
-  include puppet-tomcat-httpd-java::httpd
-  include puppet-tomcat-httpd-java::tomcat
-}
--- puppet-tomcat-httpd-java/manifests/java.pp
+++
@@ -1,6 +0,0 @@
-class puppet-tomcat-httpd-java::java {
-  package { 'java-11-openjdk':
-    ensure => installed,
-    name   => 'java-11-openjdk',
-  }
-}
